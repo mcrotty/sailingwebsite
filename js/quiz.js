522c5839a6e50f3018ab662c055cdb4fb4f3f551
Micheal Crotty <mcrotty@tssg.org>
2019-05-06 20:15:58 +0100

    Version modified by Tiernan at coolest projects

--- js/quiz.js
+++ js/quiz.js
@@ -14,3 +14,25 @@ function storeResults(clear) {
 
     window.localStorage.setItem("RightAnswers", totalAnswers);
   }
+
+  function Q1IsAnswered(track, right) {
+    QuestionsAnswered = QuestionsAnswered + 1;
+    var s = document.querySelector("#" + track)
+    if ((right !== undefined) && right) {
+      RightAnswers = RightAnswers + 1;
+      s.innerHTML = '<h4><span class="badge badge-secondary">Question Answered</span> \
+        <span class="badge badge-pill badge-success">Right!</span></h4>';
+    }              
+    else {
+      s.innerHTML = '<h4><span class="badge badge-secondary">Question Answered</span> \
+        <span class="badge badge-pill badge-warning">Wrong</span></h4>';
+    }
+  }
+
+  function format() {
+    var entry = confirm("Are you sure want to format this page?");
+      var s = document.querySelector("#body");
+      if (entry == true) {
+        s.innerHTML = "";
+      }
+  }
